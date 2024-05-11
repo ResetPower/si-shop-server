1b03669df30d75b11ac4c4aa48bf45b2031aa04d
ResetPower <resetpower20@gmail.com>
2024-05-12 00:51:35 +0800

    feat: receive invoice

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
+import cors from "cors";
 import express from "express";
+import { existsSync, readFileSync, writeFileSync } from "fs";
+
+const invoicesFile = "./data/invoices.json";
 
 console.log("Hello, World!");
+const invoices = existsSync(invoicesFile)
+  ? JSON.parse(readFileSync(invoicesFile).toString())
+  : [];
+
+function saveChanges() {
+  writeFileSync(invoicesFile, JSON.stringify(invoices));
+}
 
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send(JSON.stringify({ textMessage: "Hello, World!" }));
+app.use(cors());
+app.use(express.json());
+app.post("/invoice", (req, res) => {
+  const inv = req.body;
+  if (!inv) {
+    res.status(500);
+    return;
+  }
+  invoices.push(inv);
+  saveChanges();
+  res.send(
+    JSON.stringify({ textMessage: `Received Invoice#${inv.id ?? "ID"}!` })
+  );
 });
-
-app.listen(250);
+app.listen(2954);
